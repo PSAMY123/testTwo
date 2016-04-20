e06d66fb726145b3e32f26021baa20d8f84ec71a
WangFeiwan <wangfeiwan@chinamobo.com>
2016-04-20 15:00:00 +0800

    提交测试一

--- GitTestTwoDemo/GitTestTwoDemo/ViewController.m
+++ GitTestTwoDemo/GitTestTwoDemo/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"提交测试一");
 }
 
 - (void)didReceiveMemoryWarning {
